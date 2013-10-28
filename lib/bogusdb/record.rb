a6e750857d6c04ba256fc3961b263dfe013272fa
Bradley Smith <bradleydsmith@gmail.com>
2013-10-27 18:01:12 -0600

    trying to add array of records

--- lib/bogusdb/record.rb
+++ lib/bogusdb/record.rb
@@ -4,35 +4,44 @@ module Bogusdb
   # simulates a basic database record
   # @param [Hash] args column names and values
   class Record
+    attr_accessor :new_record
     def initialize(table_data)
-      raise TypeError unless Hash(table_data)
+      #raise TypeError unless Hash(table_data)
       @table_data = table_data
-      set_id
-      @table_data.each do |k,v|
-        self.class.send(:define_method, k.to_sym, -> { v })
-        self.class.send(:define_method, "#{k}=", -> {  })
-      end
+      #set_id
+      defaults = Hash[@table_data.map { |k, v|
+        [k.to_sym, v] 
+      }]
+      @attributes = initialize_attributes(defaults)
     end
 
     def id
       SecureRandom.random_number(100)
     end
 
+    def self.create(args)
+      rows = []
+      args.each do |arg|
+        rows << new(arg)
+      end
+      rows
+    end
+
     def attributes
-      to_hash
+      @attributes
     end
 
     # @param  [String, Symbol] table_name joined table name
     # @param  [Hash] opts column names and values
     # @return [Bogusdb::Record] 
-    def join_table(table_name, opts={})
-      self.class.send(:define_method, table_name.to_sym) do
-        self.class.new(opts)
+    def join_table(table_name, opts)
+      self.class.send(:define_method, table_name.to_sym) do 
+        self.class.create(opts)
       end
     end
 
     def inspect
-      nice_format = to_hash.collect { |k,v|
+      nice_format = attributes.collect { |k,v|
         "#{k}: #{v}"
       }.compact.join(", ")
       "#<#{self.class.name}: #{nice_format}"
@@ -40,13 +49,13 @@ module Bogusdb
 
     private
 
-    def to_hash
-      hsh = {}
-      column_names.sort.each do |m|
-        hsh[m] = self.method(m).call unless [:attributes].include? m
-      end
-      hsh
-    end
+    #def to_hash
+    #hsh = {}
+    #column_names.sort.each do |m|
+    #hsh[m] = self.method(m).call unless [:attributes].include? m
+    #end
+    #hsh
+    #end
 
     def column_names
       @table_data.keys 
@@ -55,6 +64,13 @@ module Bogusdb
     def set_id
       unless @table_data.include?("id" || :id)
         @table_data.merge!(id: id)
+      end
+    end
+
+    def initialize_attributes(options)
+      options.each do |key,value|
+        self.define_singleton_method(key)       { options[key] }
+        self.define_singleton_method("#{key}=") { |val| options[key]=val }
       end
     end
 
--- test/record_test.rb
+++ test/record_test.rb
@@ -2,17 +2,28 @@ require_relative "test_helper"
 
 class RecordTest < MiniTest::Test
   def setup
-    @bogusdb = Bogusdb::Record.new(name: 'foo', dob: '04/14/75')
+    @bogusdb = Bogusdb::Record.new(name: 'akira', dob: '04/14/75')
   end
   
   def test_responds_to_new
     assert_respond_to Bogusdb::Record, :new
   end
 
-  def test_bogusdb_has_correct_params
-    assert_raises(TypeError) {
-      Bogusdb::Record.new("some_param")
-    }
+  def test_responds_to_create
+    assert_respond_to Bogusdb::Record, :create
+  end
+
+  #def test_bogusdb_does_not_accept_string_params
+    #assert_raises(TypeError) {
+      #Bogusdb::Record.new("some_param")
+    #}
+  #end
+
+  def test_multiple_records_on_new
+    @bogusdb = Bogusdb::Record.create([{name: 'foo', dob: '04/14/75'},
+                                    {name: 'baz', dob: '01/01/00'}])
+
+    assert_equal @bogusdb.first.name, 'foo' 
   end
 
   def test_bogusdb_responds_to_name
@@ -47,12 +58,28 @@ class RecordTest < MiniTest::Test
 
   def test_joined_tabled_responds_to_columns
     @bogusdb.join_table(:profile, {avatar: 'image.jpg'})
-    assert_respond_to @bogusdb.profile, :avatar
+    assert_respond_to @bogusdb.profile.first, :avatar
   end
 
   def test_joined_table_attributes_returns_hash
+    @bogusdb = Bogusdb::Record.new(name: 'akira', dob: '04/14/75')
+    #@bogusdb.join_table(:profile, [{avatar: 'image.jpg'}, {avatar: 'sunny.jpg'}])
     @bogusdb.join_table(:profile, {avatar: 'image.jpg'})
-    assert_kind_of(Hash, @bogusdb.profile.attributes)
+    assert_kind_of(Hash, @bogusdb.profile.first.attributes)
   end
 
+  def test_joined_table_has_many
+    @bogusdb = Bogusdb::Record.new(name: 'akira', dob: '04/14/75')
+    @bogusdb.join_table(:profile, [{avatar: 'image.jpg'}])
+    assert_kind_of(Array, @bogusdb.profile)
+  end
+
+  def test_somethong
+    @bogusdb = Bogusdb::Record.create([{name: 'foo', dob: '04/14/75'},
+                                    {name: 'baz', dob: '01/01/00'}])
+
+    @bogusdb.map {|i| i.join_table(:profile, {avatar: 'me.jpg'}) }
+    p @bogusdb.first.profile
+    
+  end
 end
